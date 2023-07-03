6ddc30cdae9512a327f4bf0072ca0c9365ce1e34
Tâm Châu <thanhtamag@gmail.com>
2023-07-03 11:07:03 +0700

    Add Screen Loader

---
+++ src/components/molecules/Loader/index.tsx
@@ -0,0 +1,11 @@
+import { Spin } from 'antd';
+
+const Loader = () => {
+  return (
+    <div className="tw-w-full tw-h-full tw-flex tw-justify-center tw-items-center">
+      <Spin />
+    </div>
+  );
+};
+
+export default Loader;
